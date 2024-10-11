646e4c494b3d5a5c26fb6d28a6024d5c3babf1be
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2024-10-11 20:15:13 +0300

    w3wefwf

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,27 +1,32 @@
 import { render } from "@testing-library/react";
 import styled from "./Stats.module.css"
 import React, { Component } from "react";
-
-class Stast extends Component {
-
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0,
-    }
-    addGood = () => {
-        this.setState({ good: this.state.good + 1 })
+import { useState } from 'react';
+const  Stast = () => {
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+    // state = {
+    //     good: 0,
+    //     neutral: 0,
+    //     bad: 0,
+    // }
+   const addGood = () => {
+        // this.setState({ good: this.state.good + 1 })
+        setGood(good + 1)
         // const content = document.getElementById('content');
         // console.log(content)
         // content.classList.toggle('hidden');
     }
-    addNaturel = () => {
-        this.setState({ neutral: this.state.neutral + 1 })
+    const  addNaturel = () => {
+        // this.setState({ neutral: this.state.neutral + 1 })
+        setNeutral(neutral + 1)
     }
-    addBad = () => {
-        this.setState({ bad: this.state.bad + 1 })
+    const addBad = () => {
+        // this.setState({ bad: this.state.bad + 1 })
+        setBad(bad + 1)
     }
-    emoji = (num) => {
+    const emoji = (num) => {
         if (num >= 80 ) {
             return "🥰";
         }
@@ -39,41 +44,32 @@ class Stast extends Component {
     }
 
 
+    function positiveFeedback() {
+        let Total = good + neutral + bad;
+        const positive = good;
+        return Total === 0 ? 0 : (positive / Total) * 100;
+    }
 
+    return(
 
-    render() {
-        const { good, neutral, bad } = this.state;
-
-        function positiveFeedback() {
-            let Total = good + neutral + bad;
-            const positive = good;
-            return Total === 0 ? 0 : (positive / Total) * 100;
-        }
-
-        return (
-
+    
             <>
             <div className={styled.list}>
-            <button className={styled.btn} onClick={() => this.addGood()} >good</button>
-                <button className={styled.btn} onClick={() => this.addNaturel()}>neutral</button>
-                <button className={styled.btn} onClick={() => this.addBad()}>bad</button>
+            <button className={styled.btn} onClick={addGood} >good</button>
+                <button className={styled.btn} onClick={addNaturel}>neutral</button>
+                <button className={styled.btn} onClick={addBad}>bad</button>
             </div>
-               
+               <h1>Statistics</h1>
                 <p className={styled.text} >good: <span>{good}</span>😀</p>
                 <p className={styled.text}>neutral: <span>{neutral}</span>😑</p>
                 <p className={styled.text}>bad: <span>{bad}</span>😭</p>
                 <p className={styled.text}>Total: <span>{good + neutral + bad}</span></p>
-                <p className={styled.text}>Feedback positive: <span>{Math.trunc(positiveFeedback())}%</span>{this.emoji(Math.trunc(positiveFeedback()))}</p>
+                <p className={styled.text}>Feedback positive: <span>{Math.trunc(positiveFeedback())}%</span>{emoji(Math.trunc(positiveFeedback()))}</p>
            
-           
-                {/* <p id="content" className={styled.hhh}>😭</p> */}
-            </>
-        )
-    }
+           </>
 
+    )
 }
-
-
 export default Stast;
 
 
